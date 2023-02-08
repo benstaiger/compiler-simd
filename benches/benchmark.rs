97415a09e7367066d533304b629d6dde86f15b09
bstaiger <bhstaiger@gmail.com>
2023-02-08 07:44:33 -0600

    Add neon-specific test case

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,16 +3,29 @@ use nalgebra::DVector;
 // test ndarray as well?
 use simd::*;
 
-
 pub fn dot_benchmark(c: &mut Criterion) {
-    let x = vec![0.0; 1024*1024];
+    let x = vec![0.0; 1024 * 1024];
     c.bench_function("for loop (2^20 f32)", |b| b.iter(|| dot(black_box(&x))));
-    c.bench_function("unrolled(4) for loop (2^20 f32)", |b| b.iter(|| dot_unrolled4(black_box(&x))));
-    c.bench_function("unrolled(8) for loop (2^20 f32)", |b| b.iter(|| dot_unrolled8(black_box(&x))));
-    c.bench_function("std::simd(4) (2^20 f32)", |b| b.iter(|| dot_stdsimd4(black_box(&x), black_box(&x))));
-    c.bench_function("std::simd(8) (2^20 f32)", |b| b.iter(|| dot_stdsimd8(black_box(&x), black_box(&x))));
-    let x_nalgebra = DVector::from_element(1024*1024, 0.0);
-    c.bench_function("nalgebra", |b| b.iter(|| dot_nalgebra(black_box(&x_nalgebra), black_box(&x_nalgebra))));
+    c.bench_function("unrolled(4) for loop (2^20 f32)", |b| {
+        b.iter(|| dot_unrolled4(black_box(&x)))
+    });
+    c.bench_function("unrolled(8) for loop (2^20 f32)", |b| {
+        b.iter(|| dot_unrolled8(black_box(&x)))
+    });
+    c.bench_function("std::simd(4) (2^20 f32)", |b| {
+        b.iter(|| dot_stdsimd4(black_box(&x), black_box(&x)))
+    });
+    c.bench_function("std::simd(8) (2^20 f32)", |b| {
+        b.iter(|| dot_stdsimd8(black_box(&x), black_box(&x)))
+    });
+    c.bench_function("neon std::aarch64 (2^20 f32)", |b| {
+        b.iter(|| dot_neon(black_box(&x), black_box(&x)))
+    });
+    // c.bench_function("cblas (2^20 f32)", |b| b.iter(|| dot_cblas(black_box(&x), black_box(&x))));
+    let x_nalgebra = DVector::from_element(1024 * 1024, 0.0);
+    c.bench_function("nalgebra", |b| {
+        b.iter(|| dot_nalgebra(black_box(&x_nalgebra), black_box(&x_nalgebra)))
+    });
 }
 
 criterion_group!(benches, dot_benchmark);
--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,30 @@
 
 extern crate nalgebra as na;
 
+use cblas::sdot;
 use na::DVector;
 
-// manual vectorization
-// std::simd portable simd?
-// use neon directly?
-// https://github.com/hsivonen/simd/blob/master/examples/dot-product.rs
-// https://hugotunius.se/2017/12/01/exploring-simd-in-rust.html
-// https://docs.rs/wyrm/0.1.0/src/wyrm/numerics.rs.html#7-29
+use std::arch::aarch64::{float32x4_t, vaddvq_f32, vmlaq_f32};
+use std::mem::transmute;
+use std::simd::f32x4;
+
 pub fn dot_stdsimd4(xs: &[f32], ys: &[f32]) -> f32 {
-    let mut simd_result = std::simd::f32x4::splat(0.0);
+    let mut simd_result = f32x4::splat(0.0);
     let mut scalar_result = 0.0;
-    let stride = 8;
+    let stride = 4;
 
     let split_idx = std::cmp::min(xs.len(), ys.len()) / stride * stride;
     let (simd_xs, scalar_xs) = xs.split_at(split_idx);
     let (simd_ys, scalar_ys) = ys.split_at(split_idx);
 
     for (x, y) in simd_xs.chunks(stride).zip(simd_ys.chunks(stride)) {
-        simd_result =
-            simd_result + std::simd::f32x4::from_slice(x) * std::simd::f32x4::from_slice(y);
+        simd_result = simd_result + f32x4::from_slice(x) * f32x4::from_slice(y);
     }
 
     for (x_scalar, y_scalar) in scalar_xs.iter().zip(scalar_ys.iter()) {
         scalar_result += x_scalar * y_scalar;
     }
 
-    // scalar_result + (0..8).map(|idx| simd_result.extract(idx)).sum::<f32>()
     scalar_result + simd_result.as_array().iter().sum::<f32>()
 }
 
@@ -50,14 +47,38 @@ pub fn dot_stdsimd8(xs: &[f32], ys: &[f32]) -> f32 {
         scalar_result += x_scalar * y_scalar;
     }
 
-    // scalar_result + (0..8).map(|idx| simd_result.extract(idx)).sum::<f32>()
     scalar_result + simd_result.as_array().iter().sum::<f32>()
 }
 
-// pub fn dot_neon(x: &[f32]) -> f32 {
-//     // call neon intrinsics directly rather than relying on portable simd from std.
-//     0f32
-// }
+#[cfg(all(target_arch = "aarch64"))]
+pub fn dot_neon(xs: &[f32], ys: &[f32]) -> f32 {
+    // To do this, we want to call vmlaq_f32 for the 4xf32 multiply accumulated.
+    let mut simd_result: float32x4_t = unsafe { transmute(f32x4::splat(0.0)) };
+    let stride = 4;
+
+    let split_idx = std::cmp::min(xs.len(), ys.len()) / stride * stride;
+    let (simd_xs, scalar_xs) = xs.split_at(split_idx);
+    let (simd_ys, scalar_ys) = ys.split_at(split_idx);
+
+    for (x, y) in simd_xs.chunks(stride).zip(simd_ys.chunks(stride)) {
+        unsafe {
+            let simdx: float32x4_t = transmute(f32x4::from_slice(x));
+            let simdy: float32x4_t = transmute(f32x4::from_slice(y));
+            simd_result = vmlaq_f32(simd_result, simdx, simdy);
+        }
+    }
+
+    let mut scalar_result = 0.0;
+    for (x_scalar, y_scalar) in scalar_xs.iter().zip(scalar_ys.iter()) {
+        scalar_result += x_scalar * y_scalar;
+    }
+
+    scalar_result + unsafe { vaddvq_f32(simd_result) }
+}
+
+pub fn dot_cblas(x: &[f32], y: &[f32]) -> f32 {
+    unsafe { sdot(x.len() as i32, x, 1, y, 1) }
+}
 
 pub fn dot_nalgebra(x: &DVector<f32>, y: &DVector<f32>) -> f32 {
     x.dot(&y)
@@ -76,7 +97,8 @@ pub fn dot_unrolled4(x: &[f32]) -> f32 {
 }
 
 pub fn dot_unrolled8(x: &[f32]) -> f32 {
-    let (mut txx1, mut txx2, mut txx3, mut txx4, mut txx5, mut txx6, mut txx7, mut txx8) = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    let (mut txx1, mut txx2, mut txx3, mut txx4, mut txx5, mut txx6, mut txx7, mut txx8) =
+        (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
     for i in (0..x.len()).step_by(8) {
         txx1 += x[i] * x[i];
         txx2 += x[i + 1] * x[i + 1];
@@ -97,4 +119,41 @@ pub fn dot(x: &[f32]) -> f32 {
         tx += x[i] * x[i];
     }
     tx
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_simple_dot() {
+        let x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        assert_eq!(dot(&x), 140.0);
+    }
+
+    #[test]
+    fn test_unrolled_dot() {
+        let x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        assert_eq!(dot(&x), dot_unrolled4(&x));
+        assert_eq!(dot(&x), dot_unrolled8(&x));
+    }
+
+    #[test]
+    fn test_stdsimd_dot() {
+        let x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        assert_eq!(dot(&x), dot_stdsimd4(&x, &x));
+        assert_eq!(dot(&x), dot_stdsimd8(&x, &x));
+    }
+
+    #[test]
+    fn test_neon_dot() {
+        let x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        assert_eq!(dot(&x), dot_neon(&x, &x));
+    }
+
+    // #[test]
+    fn test_cblas_dot() {
+        let x = vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0];
+        assert_eq!(dot(&x), dot_cblas(&x, &x));
+    }
 }
